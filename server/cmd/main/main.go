962a6c8237246f5e2e57e964b3cd1fae73f7788e
kafei <229482191@qq.com>
2021-12-30 09:03:35 +0800

    tidy flag

--- server/cmd/main/main.go
+++ server/cmd/main/main.go
@@ -4,8 +4,12 @@ import (
 	"flag"
 )
 
+var (
+	tomlPath = flag.String("toml", "../toml", "toml file path")
+)
+
 func main() {
-	flag.String("toml", "../toml", "toml file path")
+
 	flag.String("app", "all", "app type")
 	flag.Int("id", 0, "app id")
 	flag.Int("log", -1, "log level, if debug log=-1")
