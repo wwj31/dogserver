dd978a4412a8ed57df088e15a5d556f062e90c8d
wwj31 <229482191@qq.com>
2022-12-21 23:21:22 +0800

    comment of pprof and graphviz

--- server/cmd/main/pprof.go
+++ server/cmd/main/pprof.go
@@ -22,3 +22,17 @@ func pprof(port string) {
 // go tool pprof http://localhost:6060/debug/pprof/profile?seconds=30
 // go tool pprof http://localhost:6060/debug/pprof/heap?seconds=30
 // go tool pprof http://localhost:6060/debug/pprof/goroutine?seconds=30
+
+// 安装graphviz后，通过以下命令打开浏览器可以查看分析图和火焰图 6061端口可以随意定
+// go tool pprof -http localhost:6061 ./pprof.samples.cpu.001.pb.gz
+
+/*
+
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+
+brew update
+brew install graphviz
+*/
