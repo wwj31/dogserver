1f200cfe27d6f2cabfef5eed11ccc7cf3d117afd
wwj <>
2023-04-06 18:43:56 +0800

    no message

--- server/cmd/main/main.go
+++ server/cmd/main/main.go
@@ -11,8 +11,9 @@ var (
 	tomlPath = flag.String("toml", "../toml", "toml file path")
 	appName  = flag.String("app", "all", "app type")
 	appId    = flag.Int("id", 1, "app id")
-	logLevel = flag.Int("log", -1, "log level, if debug log=-1")
+	logLevel = flag.Int("log", -1, "log level, debug log=-1")
 	logPath  = flag.String("log_path", "./", "path of log file")
+	version  = flag.String("v", "0.0.1", "the version of server")
 )
 
 func main() {
