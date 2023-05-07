3e32b47d843dc9ae9ab1b47b17a2bf209cdb89f9
wwj31 <229482191@qq.com>
2023-05-07 20:50:38 +0800

    no message

--- server/proto/outermsg/win_make_gogo.bat
+++
@@ -1,4 +0,0 @@
-go run github.com/wwj31/msgidgen@latest -pack=outer -path=./proto -tag=./outer -upper=false -prefix=Id
-protoc -I=%GOPATH%/pkg/mod/github.com/gogo/protobuf@v1.3.2/protobuf -I=.\proto --gogofaster_out=.\ .\proto\*.proto
-go generate ./outer/type.go
-pause
