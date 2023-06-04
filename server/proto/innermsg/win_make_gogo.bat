2598dd9e7e0b5af8dcdf0ae36c7a9e0e73e2e99a
hty <>
2023-06-04 14:42:13 +0800

    no message

--- server/proto/innermsg/win_make_gogo.bat
+++ server/proto/innermsg/win_make_gogo.bat
@@ -1,3 +1,3 @@
-protoc -I=./proto -I=%GOPATH%\pkg\mod\github.com\gogo\protobuf@v1.3.2\gogoproto --gogofaster_out=./ -I=..\exec\  .\proto\*.proto
+..\exec\protoc.exe -I=./proto -I=%GOPATH%\pkg\mod\github.com\gogo\protobuf@v1.3.2\gogoproto --gogofaster_out=./ -I=..\exec\  .\proto\*.proto
 go generate ./inner/type.go
 pause
