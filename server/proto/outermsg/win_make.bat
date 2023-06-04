e9572d1dd9f8991ad0fc180d20b6c060611bfd8c
hty <>
2023-06-04 14:19:45 +0800

    no message

--- server/proto/outermsg/win_make.bat
+++ server/proto/outermsg/win_make.bat
@@ -1,4 +1,4 @@
 go run github.com/wwj31/msgidgen@latest -pack=outer -path=./proto -tag=./outer -upper=false -prefix=Id
-protoc -I=.\proto --plugin protoc-gen-go=../exec/protoc-gen-go.exe --go_out=./ .\proto\*.proto
+..\exec\protoc.exe -I=.\proto --plugin protoc-gen-go=../exec/protoc-gen-go.exe --go_out=./ .\proto\*.proto
 go generate ./outer/type.go
 pause
