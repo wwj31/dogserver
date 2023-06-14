7951586b00bf4bf3f16537a5e113aaae287eecb0
wangwenjie <wangwenjie3000@gmail.com>
2023-06-14 17:51:40 +0800

    no message

--- server/rdsop/room.go
+++ server/rdsop/room.go
@@ -3,6 +3,7 @@ package rdsop
 import (
 	"context"
 	"math/rand"
+	"sync"
 
 	"github.com/spf13/cast"
 
@@ -18,26 +19,35 @@ func DelRoomMgr(mgrId int32) {
 	rds.Ins.SRem(context.Background(), RoomMgrKey(), mgrId, 0)
 }
 
-var cursor uint64
-
-func init() {
-	num, err := rds.Ins.SCard(context.Background(), RoomMgrKey()).Result()
-	if num == 0 {
-		log.Warnw("room mgr num == 0", "err", err)
-		return
-	}
-	cursor = uint64(rand.Intn(int(num + 1)))
-}
+var (
+	cursor     uint64 = -1
+	cursorLock sync.Locker
+)
 
 func GetRoomMgrId() int32 {
-	result, cur, err := rds.Ins.SScan(context.Background(), RoomMgrKey(), uint64(cursor), "", 1).Result()
+	cursorLock.Lock()
+	defer cursorLock.Unlock()
+
+	if cursor == -1 {
+		num, err := rds.Ins.SCard(context.Background(), RoomMgrKey()).Result()
+		if num == 0 {
+			log.Warnw("room mgr num == 0", "err", err)
+			return -1
+		}
+		cursor = uint64(rand.Intn(int(num)))
+	}
+
+	result, cur, err := rds.Ins.SScan(context.Background(), RoomMgrKey(), cursor, "", 1).Result()
 	if err != nil {
 		log.Errorw("redis sscan room mgr failed", "cursor", cursor, "err", err)
-		return 0
+		return -1
 	}
+
 	cursor = cur
 	if len(result) > 0 {
 		return cast.ToInt32(result[0])
 	}
-	return 0
+
+	log.Errorw("can not find room mgr", "cursor", cursor)
+	return -1
 }
