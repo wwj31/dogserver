f4023e7ddac3abbe694f91ba96f0c74d5d3555fe
wwj31 <229482191@qq.com>
2023-06-24 21:14:52 +0800

    no message

--- server/service/room/mahjong/state.go
+++ server/service/room/mahjong/state.go
@@ -7,6 +7,6 @@ const (
 	Deal                      // 游戏开始发牌状态
 	Exchange3                 // 换三张状态
 	DecideIgnore              // 定缺状态
-	Play                      // 游戏状态
+	Playing                   // 游戏状态
 	Settlement                // 结算
 )
