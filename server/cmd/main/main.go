7face0cf1574380c31b9b3f5b81ec87c7b7455c5
wangwenjie <wangwenjie3000@gmail.com>
2023-12-13 16:58:20 +0800

    no message

--- server/cmd/main/main.go
+++ server/cmd/main/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 var (
-	tomlPath = flag.String("toml", "../toml", "toml file path")
-	appName  = flag.String("app", "all", "app type")
-	appId    = flag.Int("id", 1, "app id")
-	logLevel = flag.Int("log", -1, "log level, debug log=-1")
-	logPath  = flag.String("log_path", "", "path of log file")
-	version  = flag.String("v", "0.0.1", "the version of server")
+	tomlPath   = flag.String("toml", "../toml", "toml file path")
+	appName    = flag.String("app", "all", "app type")
+	appId      = flag.Int("id", 1, "app id")
+	logLevel   = flag.Int("log", -1, "log level, debug log=-1")
+	logPath    = flag.String("log_path", "", "path of log file")
+	sysLogPath = flag.String("syslog_path", "", "path of sys log file")
+	version    = flag.String("v", "0.0.1", "the version of server")
 )
 
 func main() {
