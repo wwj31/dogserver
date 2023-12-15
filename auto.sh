029ce9b689a32ae631460533d24698431b750985
wangwenjie <wangwenjie3000@gmail.com>
2023-12-15 10:26:44 +0800

    no message

--- auto.sh
+++ auto.sh
@@ -4,7 +4,7 @@ cd server/cmd/main
 GOOS=linux GOARCH=amd64 go build -o ../../../ser .
 cd -
 
-ssh dev 'sudo supervisorctl stop all; rm -rf ./server/log ./server/syslog'
+ssh dev 'sudo supervisorctl stop all; rm -rf ./server/log ./server/syslog; sudo docker-compose restart etcd'
 scp ser dev:~/server
 ssh dev 'sudo supervisorctl start all'
 rm -rf ser
